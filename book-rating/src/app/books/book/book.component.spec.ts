92fabbe4b3af985d581df52ccf0f8123de7b3c30
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-10-17 22:16:17 +0200

    testing with behaviour with jasmine spies

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -1,14 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
+import { BookRatingService } from '../shared/book-rating.service';
 
 describe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
 
+  const ratingMock = {
+    rateUp: () => { },
+    rateUpAllowed: () => { },
+    rateDownAllowed: () => { }
+  };
+
   beforeEach(async(() => {
+
+    spyOn(ratingMock, 'rateUp');
+
     TestBed.configureTestingModule({
-      declarations: [ BookComponent ]
+      declarations: [ BookComponent ],
+      providers: [
+        {
+          provide: BookRatingService,
+          useValue: ratingMock
+        }
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +32,22 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+    component.book = {
+      isbn: '000',
+      title: '12313',
+      description: '123123',
+      rating: 0
+    };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should forward the rateUp call to the book rating service', () => {
+    component.rateUp();
+    expect(ratingMock.rateUp).toHaveBeenCalled();
+    expect(ratingMock.rateUp).not.toHaveBeenCalledTimes(2);
   });
 });
