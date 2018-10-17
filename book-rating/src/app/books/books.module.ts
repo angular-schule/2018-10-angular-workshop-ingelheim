a70175039d75b47cffc039764e48205265e7c61f
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-10-17 14:18:42 +0200

    moving to bookmodule, testing the book rating service

---
+++ book-rating/src/app/books/books.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BooksRoutingModule } from './books-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BookComponent } from './book/book.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BooksRoutingModule
+  ],
+  declarations: [
+    DashboardComponent,
+    BookComponent
+  ],
+  exports: [
+    DashboardComponent
+  ],
+  providers: []
+})
+export class BooksModule { }
---
+++ book-rating/src/app/books/shared/book-rating.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Book } from './book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookRatingService {
+
+  private minRating = 1;
+  private maxRating = 5;
+
+  rateUp(book: Book) {
+    return {
+      ...book,
+      rating: Math.min(this.maxRating, book.rating + 1)
+    };
+  }
+
+  rateDown(book: Book) {
+    return {
+      ...book,
+      rating: book.rating > this.minRating ? book.rating - 1 : this.minRating
+    };
+  }
+}
