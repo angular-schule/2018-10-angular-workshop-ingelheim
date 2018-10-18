2c025237e7b3652353e4657a5163eac7008c7787
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-10-18 10:08:30 +0200

    my first e2e test

---
+++ book-rating/e2e/src/angular-buch.e2e-spec.ts
@@ -0,0 +1,18 @@
+import { browser, element, $ } from 'protractor';
+
+fdescribe('Angular Buch', () => {
+
+  beforeAll(() => browser.waitForAngularEnabled(false));
+
+  it('should just call it Angular', () => {
+
+    browser.get('https://www.dpunkt.de/buecher/12400/9783864903571-angular.html');
+    const heading = $('h1');
+    expect(heading.getText()).toEqual('Angular');
+
+  });
+
+  afterAll(() => browser.waitForAngularEnabled(true));
+
+
+});
